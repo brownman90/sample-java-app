12de014c22d8734f78687327a409322e4f63ba33
Nick Carroll <nlcarroll@gmail.com>
2012-08-07 22:25:17 +1000

    Removed Spring web from pom.

--- src/test/java/me/nickcarroll/app/service/HelloWorldServiceTest.java
+++ src/test/java/me/nickcarroll/app/service/HelloWorldServiceTest.java
@@ -1,6 +1,5 @@
 package me.nickcarroll.app.service;
 
-import me.nickcarroll.sample.service.HelloWorldService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.runners.MockitoJUnitRunner;
